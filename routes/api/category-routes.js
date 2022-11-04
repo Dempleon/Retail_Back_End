fa025574d073300229104b2e62aeec58d23c59d8
Dempoleon <dempoleon@gmail.com>
2022-11-03 18:10:28 -0700

    implemented model relationships

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.send('Category get root route');
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  res.send('Category get root :id route');
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  res.send('Category post root route');
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  res.send('Category put root :id route');
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  res.send('Category delete route');
 });
 
 module.exports = router;
